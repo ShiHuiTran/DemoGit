afa19a52ec2a9bfaad6f1030e6bad4e4f2931498
ShiHuiTran <shiiroiYume@gmail.com>
2015-01-22 23:36:30 -0300

    Cloned a line....

--- src/demogit/DemoGit.java
+++ src/demogit/DemoGit.java
@@ -15,7 +15,13 @@ public class DemoGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello " + args[0]+ "!");
+        if(args.length != 1){
+            System.out.println("Wrong args");
+            System.exit(1);
+        }
+        
+        System.out.println("Hello " + args[0] + "!");
+        System.out.println("Hello " + args[0] + "!");
     }
     
 }
